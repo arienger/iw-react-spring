e5c984c4807b99920f20f12287d3094f920e049d
arienger <arienger@gmail.com>
2024-09-30 14:17:18 +0200

    second

---
+++ src/test/java/com/example/iwjavaspringhtmx/IwReactSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iwjavaspringhtmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwReactSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
